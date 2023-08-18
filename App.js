1f4f3564a22b3426fe06dbdddadf6761f41c7ea7
Dinesh <dineshbendre1@gmail.com>
2023-08-18 22:41:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+const heading1 =React.createElement("h1",{id:"heading1",key:"first"},"Namaste Everyone");
+const heading2=React.createElement("h2",{id:"heading2",key:"second"},"Hello sablog");
+const conatainer=React.createElement("div",{id:"container"},[heading1,heading2]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(conatainer);
